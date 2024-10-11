6ae0e884902db9fc30a74818a15f118443a9c8c8
纪宇航 <jiyuhang@jctrans.net>
2024-10-11 19:37:00 +0800

    feat: add pinia, seo, image module

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="text-red-400">
     home page<Icon name="uil:github" style="color: black" />
+
+    <h1>count: {{ store.count }}</h1>
+    <button class="border p-2" @click="store.increment">add count</button>
+
+    <NuxtImg
+      src="https://resources.jctrans.com/res/hzh/pc/img/logo_nor@2x.png"
+    />
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  const store = useCounterStore()
+</script>
 
 <style></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,6 +16,12 @@ export default defineNuxtConfig({
 
     '@nuxtjs/fontaine', // 优化 Web 字体加载
     '@nuxtjs/critters', // 优化关键 CSS 加载
+
+    '@nuxt/image',
+    '@nuxtjs/seo',
+
+    '@pinia/nuxt',
+    '@pinia-plugin-persistedstate/nuxt',
   ],
 
   eslint: {
@@ -43,5 +49,43 @@ export default defineNuxtConfig({
     config: {
       preload: 'swap',
     },
+  },
+  robots: {
+    groups: [
+      {
+        userAgent: ['AdsBot-Google-Mobile', 'AdsBot-Google-Mobile-Apps'],
+        disallow: ['/admin'],
+        allow: ['/admin/login'],
+        comment:
+          'Allow Google AdsBot to index the login page but no-admin pages',
+      },
+    ],
+  },
+
+  image: {
+    format: ['webp, png, jpg'],
+    providers: {
+      ipx: {
+        sharp: {
+          webp: {
+            quality: 120,
+          },
+        },
+      },
+    },
+    // dir: 'assets/images',
+    presets: {
+      avatar: {
+        modifiers: {
+          format: 'webp',
+          width: 100,
+          height: 100,
+        },
+      },
+    },
+  },
+
+  pinia: {
+    storesDirs: ['app/stores/**'],
   },
 })
